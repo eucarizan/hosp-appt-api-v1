f2415e99c217fa7b609dcc70aa876f968e6922f6
eucarizan <eucarizan@gmail.com>
2025-11-21 19:21:26 +0800

    init commit

---
+++ habs/src/main/java/dev/nj/habs/HabsApplication.java
@@ -0,0 +1,13 @@
+package dev.nj.habs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabsApplication.class, args);
+	}
+
+}
---
+++ habs/src/test/java/dev/nj/habs/HabsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nj.habs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
