3a291b3dcd98f4945a53d597a2fcd945cd21be85
eucarizan <eucarizan@gmail.com>
2025-11-21 20:08:43 +0800

    test(appointment): add createAppointment service test - FAILING

---
+++ src/test/java/dev/nj/habs/appointment/AppointServiceTest.java
@@ -0,0 +1,39 @@
+package dev.nj.habs.appointment;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class AppointServiceTest {
+
+    @Mock
+    AppointmentRepository appointmentRepository;
+
+    @InjectMocks
+    AppointmentService appointmentService;
+
+    @Test
+    void createAppointment_validRequest_returnsAppointmentResponse() {
+        Appointment appointment = new Appointment("dr. house", "john doe", LocalDate.of(2021, 12, 1));
+        AppointmentRequest request = new AppointmentRequest("Dr. House", "John Doe", LocalDate.of(2021, 12, 1));
+        when(appointmentRepository.save(any(Appointment.class))).thenReturn(appointment);
+
+        AppointmentResponse response = appointmentService.createAppointment(request);
+
+        assertNotNull(response);
+        assertEquals(1L, response.idApp());
+        assertEquals("dr. house", response.doctor());
+        assertEquals("john doe", response.patient());
+        assertEquals(LocalDate.of(2021, 12, 1), response.date());
+    }
+}
